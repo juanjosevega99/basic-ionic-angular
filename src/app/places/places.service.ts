ad421c14235d2d37e00a841035bd710544e609af
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-07-02 07:32:00 -0500

    basic crud finished

--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
+import { Place } from './place.model'
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlacesService {
 
-  private places = [
+  private places: Place[] = [
     {
       id: '1',
       title: 'Eiffel Tower',
@@ -38,6 +39,17 @@ export class PlacesService {
       })
     }
   }
-  addPlace() {}
-  deletePlace() {}
+  addPlace(title: string, imageURL: string) {
+    this.places.push({
+      id: this.places.length + 1 + "",
+      title,
+      imageURL,
+      comments: []
+    })
+  }
+  deletePlace(placeId: string) {
+    this.places = this.places.filter(place => {
+      return place.id !== placeId
+    })
+  }
 }
