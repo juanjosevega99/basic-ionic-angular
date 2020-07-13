40e56f436cab4d812284f3e46050bf6439b7586d
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-07-13 07:33:36 -0500

    alert are you sure you want delete

--- src/app/places/place-detail/place-detail.page.ts
+++ src/app/places/place-detail/place-detail.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router'
 import { PlacesService } from '../places.service';
+import { AlertController } from '@ionic/angular'
 import { Place } from '../place.model';
 
 @Component({
@@ -12,7 +13,7 @@ export class PlaceDetailPage implements OnInit {
 
   place: Place;
   
-  constructor(private activatedRoute: ActivatedRoute, private placesService: PlacesService, private router: Router) { }
+  constructor(private activatedRoute: ActivatedRoute, private placesService: PlacesService, private router: Router, private alertCtrl: AlertController) { }
 
   ngOnInit() {
     this.activatedRoute.paramMap.subscribe(paramMap => {
@@ -22,9 +23,25 @@ export class PlaceDetailPage implements OnInit {
     })
   }
 
-  deletePlace() {
-    this.placesService.deletePlace(this.place.id)
-    this.router.navigate(['/places'])
+  async deletePlace() {
+    const alertElement = await this.alertCtrl.create({
+      header: 'Are you sure, you want to delete it?',
+      message: 'Be careful',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        }, 
+        {
+          text: 'Delete',
+          handler: () => {
+            this.placesService.deletePlace(this.place.id)
+            this.router.navigate(['/places'])
+          }
+        }
+      ]
+    })
+    await alertElement.present()
   }
 
 }
